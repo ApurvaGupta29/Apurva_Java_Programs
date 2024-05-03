94cce296c824c33bb3cd949c61e24b4484255d42
Apurva Gupta <apgupta@q3infotech.net>
2024-05-03 21:45:44 +0530

    my first commit

---
+++ ArrayClass.java
@@ -0,0 +1,16 @@
+package Array;
+public class ArrayClass {
+
+	public static void main(String[] args) {
+		String god[]=new String[5];
+		god[0]="Ram";
+		god[1]="Laxman";
+		god[2]="Shiv";
+		god[3]="Sita";
+		god[4]="Pooja";
+		for(int i=0;i<=4;i++)
+		{
+		System.out.println(god[i]);
+	}
+}
+}
---
+++ Constructor1.java
@@ -0,0 +1,26 @@
+package package1;
+
+public class Constructor1 
+	
+	{
+		Constructor1()
+	
+		{
+		System.out.println(" My First COnstructor Programm");
+	}
+
+static void add()
+{
+	System.out.println(" My Second COnstructor Programm");
+}
+void sub()
+{  
+	System.out.println(" My Third COnstructor Programm");
+}
+	public static void main(String[] args) {
+		
+		new Constructor1();
+		
+
+	}
+	}
